5469a965f5c11fb1ffcc91ff2ef4f9740cf3768f
Polina Shmeleva <polsho1988@gmail.com>
2023-10-16 01:07:05 +0300

    added cards scripts

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,40 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
+function getCard(cardInfo, deleteCard) {
+  const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: Функция создания карточки
+  const cardItem = cardTemplate.querySelector('.card').cloneNode(true);
+  const removeButton = cardItem.querySelector('.card__delete-button');
 
-// @todo: Функция удаления карточки
+  cardItem.querySelector('.card__image').src = cardInfo.link;
+  cardItem.querySelector('.card__title').textContent = cardInfo.name;
 
-// @todo: Вывести карточки на страницу
+  removeButton.addEventListener('click', deleteCard);
+
+  return cardItem;
+}
+
+function deleteCard(event) {
+    event.target.closest('.card').remove();
+}
+
+const cardList = document.querySelector('.places__list');
+
+initialCards.forEach(function (card) {
+  cardList.append(getCard(card, deleteCard));
+});
+
+
+const editProfileButton = document.querySelector('.profile__edit-button');
+editProfileButton.addEventListener('click', function(event) {
+    document.querySelector('.popup_type_edit').classList.add('popup_is-opened');
+});
+
+const newCardButton = document.querySelector('.profile__add-button');
+newCardButton.addEventListener('click', function(event) {
+    document.querySelector('.popup_type_new-card').classList.add('popup_is-opened');
+});
+
+const closePopupButton = document.querySelector('.popup__close');
+closePopupButton.addEventListener('click', function(event) {
+    event.target.closest('.popup').classList.remove('popup_is-opened');
+});
