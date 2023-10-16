bd91dc67c0582e2edc5231b4b4641318c6bf1388
Polina Shmeleva <polsho1988@gmail.com>
2023-10-16 23:00:15 +0300

    added popup-image

--- scripts/index.js
+++ scripts/index.js
@@ -9,9 +9,7 @@ function getCard(cardInfo, deleteCard) {
   cardItem.querySelector(".card__title").textContent = cardInfo.name;
 
   removeButton.addEventListener("click", deleteCard);
-  likeButton.addEventListener('click', function() {
-
-  })
+  likeButton.addEventListener("click", function () {});
 
   return cardItem;
 }
@@ -26,23 +24,45 @@ initialCards.forEach(function (card) {
   cardList.append(getCard(card, deleteCard));
 });
 
-
 const editProfileButton = document.querySelector(".profile__edit-button");
 editProfileButton.addEventListener("click", function (event) {
-  document.querySelector(".popup_type_edit").classList.add('popup_is-opened', 'popup_is-animated');
+  document
+    .querySelector(".popup_type_edit")
+    .classList.add("popup_is-animated", "popup_is-opened");
 });
-
 
 const newCardButton = document.querySelector(".profile__add-button");
 newCardButton.addEventListener("click", function (event) {
   document
     .querySelector(".popup_type_new-card")
-    .classList.add("popup_is-opened", 'popup_is-animated');
+    .classList.add("popup_is-animated", "popup_is-opened");
 });
 
-const closePopupButtons = Array.from(document.querySelectorAll(".popup__close"));
+const cardImageButtons = Array.from(
+  document.querySelectorAll(".card__image")
+);
+cardImageButtons.forEach(function (btn) {
+  btn.addEventListener("click", function (event) {
+    const popupImage = document.querySelector(".popup_type_image");
+    popupImage.classList.add("popup_is-opened");
+    popupImage.querySelector('.popup__image').src = btn.src;
+  });
+});
+
+const closePopupButtons = Array.from(
+  document.querySelectorAll(".popup__close")
+);
 closePopupButtons.forEach(function (btn) {
   btn.addEventListener("click", function (event) {
     event.target.closest(".popup").classList.remove("popup_is-opened");
+  });
+});
+
+const cardLikeButtons = Array.from(
+  document.querySelectorAll(".card__like-button")
+);
+cardLikeButtons.forEach(function (btn) {
+  btn.addEventListener("click", function (event) {
+    event.target.classList.add("card__like-button_is-active");
   });
 });
